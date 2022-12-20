ec09b3712b6ca2580f09f8506aeabfecab71e683
nitishdsharma <nsharma@catalogicsoftware.com>
2022-12-20 19:15:33 +0530

    Adding updated Testcase file

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -4,65 +4,32 @@
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
 import unittest
-from unittest.mock import Mock
+from unittest.mock import patch
+# from unittest.mock import Mock
 
 from charm import CloudcasaCharm
-from ops.model import ActiveStatus
+# from ops.model import ActiveStatus
 from ops.testing import Harness
 
 
 class TestCharm(unittest.TestCase):
     def setUp(self):
+        self.clusterid = "6qw77af945675b1327d0bc5ee"
         self.harness = Harness(CloudcasaCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_config_changed(self):
-        self.assertEqual(list(self.harness.charm._stored.things), [])
-        self.harness.update_config({"thing": "foo"})
-        self.assertEqual(list(self.harness.charm._stored.things), ["foo"])
+    def test_clusterid(self):
+        # Test with empty clusterid.
+        self.assertEqual(self.harness.charm.config["clusterid"], "")
+        self.harness.disable_hooks()
+        # Test with some dummy clusterid
+        self.harness.update_config({"clusterid": self.clusterid})
+        self.assertEqual(len(self.harness.charm.config["clusterid"]), 25)
 
-    def test_action(self):
-        # the harness doesn't (yet!) help much with actions themselves
-        action_event = Mock(params={"fail": ""})
-        self.harness.charm._on_fortune_action(action_event)
+    @patch("charm.CloudcasaCharm._create_kubernetes_resources")
+    def test_on_install(self, _create_kubernetes_resources):
+        # Test with not install.
+        self.harness.charm._on_install("mock_event")
+        _create_kubernetes_resources.assert_called_once()
 
-        self.assertTrue(action_event.set_results.called)
-
-    def test_action_fail(self):
-        action_event = Mock(params={"fail": "fail this"})
-        self.harness.charm._on_fortune_action(action_event)
-
-        self.assertEqual(action_event.fail.call_args, [("fail this",)])
-
-    def test_httpbin_pebble_ready(self):
-        # Simulate making the Pebble socket available
-        self.harness.set_can_connect("httpbin", True)
-        # Check the initial Pebble plan is empty
-        initial_plan = self.harness.get_container_pebble_plan("httpbin")
-        self.assertEqual(initial_plan.to_yaml(), "{}\n")
-        # Expected plan after Pebble ready with default config
-        expected_plan = {
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": "🎁"},
-                }
-            },
-        }
-        # Get the httpbin container from the model
-        container = self.harness.model.unit.get_container("httpbin")
-        # Emit the PebbleReadyEvent carrying the httpbin container
-        self.harness.charm.on.httpbin_pebble_ready.emit(container)
-        # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan("httpbin").to_dict()
-        # Check we've got the plan we expected
-        self.assertEqual(expected_plan, updated_plan)
-        # Check the service was started
-        service = self.harness.model.unit.get_container("httpbin").get_service("httpbin")
-        self.assertTrue(service.is_running())
-        # Ensure we set an ActiveStatus with no message
-        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
